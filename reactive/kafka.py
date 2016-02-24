5800fd7f0eae5637b0d68174968b637611cf6c93
Kevin W Monroe <kevin.monroe@canonical.com>
2016-02-24 15:59:25 +0000

    lint fixes, refactor DistConfig, move Kafka class to charms.layer

--- reactive/kafka.py
+++ reactive/kafka.py
@@ -1,22 +1,17 @@
-import jujuresources
 from charms.reactive import when, when_not
 from charms.reactive import set_state, remove_state
+
 from charmhelpers.core import hookenv
-from charms.kafka import Kafka
+
+from charms.layer.kafka import Kafka
+
 from jujubigdata.utils import DistConfig
-
-def dist_config():
-
-    if not getattr(dist_config, 'value', None):
-        kafka_reqs = ['vendor', 'packages', 'groups', 'users', 'dirs', 'ports']
-        dist_config.value = DistConfig(filename='dist.yaml', required_keys=kafka_reqs)
-    return dist_config.value
 
 
 @when_not('kafka.installed')
 def install_kafka(*args):
 
-    kafka = Kafka(dist_config())
+    kafka = Kafka(DistConfig())
     if kafka.verify_resources():
         hookenv.status_set('maintenance', 'Installing Kafka')
         kafka.install()
@@ -42,28 +37,28 @@ def configure_kafka(zkjoining, zkavailable):
     try:
         zk_units = zkavailable.get_zookeeper_units()
         hookenv.status_set('maintenance', 'Setting up Kafka')
-        kafka = Kafka(dist_config())
+        kafka = Kafka(DistConfig())
         kafka.configure_kafka(zk_units)
         kafka.start()
         zkjoining.dismiss_joining()
         hookenv.status_set('active', 'Ready')
         set_state('kafka.started')
     except:
-        hookenv.log("Relation with Zookeeper not established")        
-    
+        hookenv.log("Relation with Zookeeper not established")
+
 
 @when('kafka.started', 'zookeeper.joining', 'zookeeper.available')
 def reconfigure_kafka_new_zk_instances(zkjoining, zkavailable):
     try:
         zk_units = zkavailable.get_zookeeper_units()
         hookenv.status_set('maintenance', 'Updating Kafka with new Zookeeper instances')
-        kafka = Kafka(dist_config())
+        kafka = Kafka(DistConfig())
         kafka.configure_kafka(zk_units)
         kafka.restart()
         zkjoining.dismiss_joining()
         hookenv.status_set('active', 'Ready')
     except:
-        hookenv.log("Relation with Zookeeper not established")        
+        hookenv.log("Relation with Zookeeper not established")
 
 
 @when('kafka.started', 'zookeeper.departing', 'zookeeper.available')
@@ -71,14 +66,14 @@ def reconfigure_kafka_zk_instances_leaving(zkdeparting, zkavailable):
     try:
         zk_units = zkavailable.get_zookeeper_units()
         hookenv.status_set('maintenance', 'Updating Kafka with departing Zookeeper instances ')
-        kafka = Kafka(dist_config())
+        kafka = Kafka(DistConfig())
         kafka.configure_kafka(zk_units)
         kafka.restart()
         zkdeparting.dismiss_departing()
         hookenv.status_set('active', 'Ready')
     except:
-        hookenv.log("Relation with Zookeeper not established. Stopping Kafka.")        
-        kafka = Kafka(dist_config())
+        hookenv.log("Relation with Zookeeper not established. Stopping Kafka.")
+        kafka = Kafka(DistConfig())
         kafka.stop()
         remove_state('kafka.started')
         hookenv.status_set('blocked', 'Waiting for connection to Zookeeper')
@@ -87,7 +82,6 @@ def reconfigure_kafka_zk_instances_leaving(zkdeparting, zkavailable):
 @when('kafka.connected')
 @when_not('kafka.available')
 def waiting_availuable_flume(kafka_client):
-    port = dist_config().exposed_ports('kafka')[0]
-    kafka_client.send_configuration(port)    
+    port = DistConfig().exposed_ports('kafka')[0]
+    kafka_client.send_configuration(port)
     hookenv.log('Sending configuration to client')
-
